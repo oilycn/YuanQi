5713c24cbae61991658215e26ad770d2b8388bf9
jinsama <47803745+Aiyuanjiang@users.noreply.github.com>
2019-06-24 20:30:42 +0800

    Update wgclients.sh

--- wgclients.sh
+++ wgclients.sh
@@ -9,20 +9,6 @@ ip_list=(2 5 8 178 186 118 158 198 168 9)
 ipv6_range="fd08:620c:4df0:65eb::"
 
 #############################################################
-help_info() {
-cat  <<EOF
-# 一键安装wireguard 脚本 Debian 9 (源:逗比网安装笔记)
-wget -qO- git.io/wireguard | bash
-
-# 一键安装wireguard 脚本 Ubuntu   (源:逗比网安装笔记)
-wget -qO- git.io/jinwgununtu | bash
-
-# CentOS 7 一键脚本安装WireGuard  (官方脚本自动升级内核)
-wget -qO- git.io/jinwgcentos7 | bash
-
-EOF
-}
-#############################################################
 
 if [[ $# > 0 ]]; then
     num="$1"
@@ -36,14 +22,6 @@ Green="\033[32m"  && Red="\033[31m" && GreenBG="\033[42;37m" && RedBG="\033[41;3
 
 # 定义提示信息
 Info="${Green}[信息]${Font}"  &&  OK="${Green}[OK]${Font}"  &&  Error="${Red}[错误]${Font}"
-
-# 检查是否安装 WireGuard
-if [ ! -f '/usr/bin/wg' ]; then
-    clear
-    help_info
-    echo -e "${Red}::  检测到你的vps没有安装wireguard，请选择复制一键脚本安装   ${Font}"
-    exit 1
-fi
 
 host=$(hostname -s)
 # 获得服务器ip，自动获取
@@ -61,8 +39,6 @@ if [ ! -f '/usr/bin/qrencode' ]; then
     apt -y install qrencode
 fi
 
-# 安装 bash wgmtu 脚本用来设置服务器
-wget -O ~/wgluan  https://git.io/wgluan
 #############################################################
 
 # 打开ip4/ipv6防火墙转发功能
@@ -111,9 +87,9 @@ cat <<EOF >client.conf
 PrivateKey = $(cat cprivatekey)
 Address = 10.0.0.188/24,  ${ipv6_range}188/64
 DNS = 8.8.8.8, 2001:4860:4860::8888
-#  MTU = $mtu
-#  PreUp =  start   .\route\routes-up.bat
-#  PostDown = start  .\route\routes-down.bat
+MTU = $mtu
+PreUp =  start   .\route\routes-up.bat
+PostDown = start  .\route\routes-down.bat
 
 [Peer]
 PublicKey = $(cat spublickey)
@@ -163,7 +139,7 @@ fi
 wg-quick down wg0
 wg-quick up wg0
 
-# 安装 bash wg5 命令，新手下载客户端配置用
+# 安装 bash wgjin 命令，新手下载客户端配置用
 conf_url=http://${serverip}:8000
 cat  <<EOF > ~/wgjin
 next() {
@@ -173,7 +149,7 @@ next() {
 host=$(hostname -s)
 cd  /etc/wireguard/
 tar cvf  wgclients.tar  client*  wg*
-echo -e  "${GreenBG}#  Windows 客户端配置，请复制配置文本 ${Font}"
+#echo -e  "${GreenBG}#  Windows 客户端配置，请复制配置文本 ${Font}"
 
 cat /etc/wireguard/client.conf       && next
 cat /etc/wireguard/wg_${host}_2.conf   && next
@@ -182,7 +158,7 @@ cat /etc/wireguard/wg_${host}_4.conf   && next
 
 echo
 echo -e "# ${Info} 新手使用 bash wgjin ${Font} 命令，使用临时网页下载配置和手机客户端二维码配置"
-echo -e "# ${Info} 推荐使用 bash wgluan ${Font} 命令，WireGuard 配置管理支持IPV6，稳定有待测试"
+#echo -e "# ${Info} 推荐使用 bash wgluan ${Font} 命令，WireGuard 配置管理支持IPV6，稳定有待测试"
 echo -e "# ${Info} 自定端口 bash <(curl -L -s https://git.io/fpnQt) 9999 ${Font}"
 
 
