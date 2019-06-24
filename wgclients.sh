11db3174f8a103aef4544b9989242f8b3731b1f4
jinsama <47803745+Aiyuanjiang@users.noreply.github.com>
2019-06-24 19:44:18 +0800

    Update wgclients.sh

--- wgclients.sh
+++ wgclients.sh
@@ -137,7 +137,7 @@ wg-quick down wg0
 wg-quick up wg0
 
 # 安装 bash wgjin 命令，新手下载客户端配置用
-conf_url=http://${serverip}:8000
+conf_url=http://${serverip}:1127
 cat  <<EOF > ~/wgjin
 next() {
     printf "# %-70s\n" "-" | sed 's/\s/-/g'
@@ -145,13 +145,12 @@ next() {
 
 host=$(hostname -s)
 cd  /etc/wireguard/
-tar cvf  wgclients.tar  client*  wg*
-echo -e  "${GreenBG}#  Windows 客户端配置，请复制配置文本 ${Font}"
+tar cvf  wgclients.tar  client*  wg_*
+cat /etc/wireguard/client$i.conf | qrencode -o - -t ansi256
+#echo "# 手机扫描二维码2号配置，PC使用配置复制下面文本"
 
 cat /etc/wireguard/client.conf       && next
-cat /etc/wireguard/wg_${host}_2.conf   && next
-cat /etc/wireguard/wg_${host}_3.conf   && next
-cat /etc/wireguard/wg_${host}_4.conf   && next
+cat /etc/wireguard/client$i.conf   && next
 
 echo -e "# ${Info} 新手使用${GreenBG} bash wgjin ${Font} 命令，使用临时网页下载配置和手机客户端二维码配置"
 echo -e "# ${Info} 自定端口${GreenBG} bash <(curl -L -s https://git.io/jinwgmore) 9999 ${Font}"
@@ -159,8 +158,8 @@ echo -e "# ${Info} 自定端口${GreenBG} bash <(curl -L -s https://git.io/jinwg
 # python -m SimpleHTTPServer 8000 &
 # echo ""
 # echo -e "# ${Info} 访问 ${GreenBG}${conf_url}${Font} 点PNG二维码， ${RedBG}手机扫描二维码后请立即重启VPS。${Font}"
-echo -e "# ${Info} 客户端配置 AllowedIPs = 0.0.0.0/0, ::0/0 改成${RedBG} AllowedIPs = 10.0.0.1/32 ${Font}"
-echo -e "# ${Info} WG服务器相当于一个远程路由器，IP: 10.0.0.1,再开启一个SS，IP填 ${RedBG} IP: 10.0.0.1 ${Font}"
+# echo -e "# ${Info} 客户端配置 AllowedIPs = 0.0.0.0/0, ::0/0 改成${RedBG} AllowedIPs = 10.0.0.1/32 ${Font}"
+# echo -e "# ${Info} WG服务器相当于一个远程路由器，IP: 10.0.0.1,再开启一个SS，IP填 ${RedBG} IP: 10.0.0.1 ${Font}"
 
 
 EOF
