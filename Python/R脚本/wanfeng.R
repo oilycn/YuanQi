acb6cdb763af1096f0a0bc88be24bfe65de6bd43
wfpas <47803745+wfpas@users.noreply.github.com>
2021-09-30 12:04:27 +0800

    w

--- Python/R脚本/wanfeng.R
+++ Python/R脚本/wanfeng.R
@@ -1,7 +1,14 @@
-table <- data.frame(
-    姓名 = c("张三", "李四"),
-    工号 = c("001", "002"),
-    月薪 = c(1000, 2000)
-)
-# 显示概要
-print(summary(table))
+# 数据准备
+info <- c(1, 2, 4, 8)
+
+# 命名
+names <- c("Google", "Runoob", "Taobao", "Weibo")
+
+# 涂色（可选）
+cols <- c("#ED1C24", "#22B14C", "#FFC90E", "#3f48CC")
+# 计算百分比
+piepercent <- paste(round(100 * info / sum(info)), "%")
+# 绘图
+pie(info, labels = piepercent, main = "网站分析", col = cols, family = "GB1")
+# 添加颜色样本标注
+legend("topright", names, cex = 0.8, fill = cols)
